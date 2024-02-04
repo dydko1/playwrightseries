31b843998a24ac0a5bb38918095d648e2949becb
dydko <miroslaw.dyduch@gmail.com>
2024-02-04 08:54:17 +0100

    first commit

---
+++ src/main/java/com/dydko/Main.java
@@ -0,0 +1,22 @@
+package com.dydko;
+
+import com.microsoft.playwright.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Playwright playwright = Playwright.create();
+        ArrayList<String> arguments = new ArrayList<>();
+        arguments.add("--start-maximized");
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setArgs(arguments));
+        BrowserContext browserContext = browser.newContext(new Browser.NewContextOptions().setViewportSize(null));
+        Thread.sleep(1000);
+        Page page = browserContext.newPage();
+        page.navigate("https://www.onet.pl/");
+        System.out.println(page.title());
+        page.close();
+        browser.close();
+    }
+}
